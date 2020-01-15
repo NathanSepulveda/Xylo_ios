1bad734d48448b24470fdab846f9704adc44dfff
nathan sepulveda <nathans.composer@gmail.com>
2020-01-15 13:52:41 -0600

    initially uploaded wrong repo

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -1,16 +1,14 @@
 //
 //  ViewController.swift
 //  Xylophone
-//
-//  Created by Angela Yu on 27/01/2016.
-//  Copyright © 2016 London App Brewery. All rights reserved.
-//
+
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate{
     
-
+    var audioPlayer : AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,12 +16,30 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        let currentColor = sender.backgroundColor
+        let correctTag = sender.tag
+        sender.backgroundColor = UIColor.gray
         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+            sender.backgroundColor = currentColor
+        }
+        playSound(sender)
         
-        
+
     }
     
-  
+    func playSound(_ s: UIButton) {
+        let soundURL = Bundle.main.url(forResource: "note" + String(s.tag), withExtension: "wav")
+        
+        do {
+            print(s.tag)
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        audioPlayer.play()
+    }
 
 }
 
